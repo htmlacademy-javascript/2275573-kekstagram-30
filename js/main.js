3e83ed3331a1a34c590ab6df55d7954c4a3a213b
Anna Ignatova <uventa2@yandex.ru>
2023-11-25 02:32:49 +0300

    Правка 12.1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+import { renderGallery } from './gallery/thumbnails.js';
+import { getPictures } from './api/api.js';
+import './form/form.js';
+import { showErrorMessage } from './utils/utils.js';
+import { initFilter } from './gallery/filter.js';
+
+async function bootstrap() {
+  try {
+    const pictures = await getPictures();
+    renderGallery(pictures);
+    initFilter(pictures);
+  } catch (error) {
+    showErrorMessage();
+  }
+}
+
+bootstrap();
